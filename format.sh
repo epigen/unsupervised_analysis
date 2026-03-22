3ea09b2901b7dd68d5bb8ba9e6b5fc384a6944bf
sreichl <reichl.stephan@gmail.com>
2026-03-22 13:35:45 +0100

    move linting solution to MrBiomics

--- format.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate mrbiomics-formatting
-
-snakefmt .
-black .
-npx prettier . --write
-Rscript -e 'styler::style_dir("workflow/scripts")'
